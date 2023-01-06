9a7f69aaf1216fd2bf174fa9932a62c30cfee1bc
Angela Witham <ela.witham@gmail.com>
2023-01-06 15:51:43 -0700

    updated variables in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
+var startBtn = document.getElementById("start");
+var clock = document.getElementById("clock");
+var intro = document.getElementById("intro");
+var timeOnClock = 60;
+
+
+//Question 1//
+var question1 = document.getElementById("question1");
+var correct1 = document.getElementById("correct1");
+var incorrect1 = document.getElementById("incorrect1");
+
+//Question 2//
+var question2 = document.getElementById("question2");
+var correct2 = document.getElementById("correct2");
+var incorrect2 = document.getElementById("incorrect2");
+
+//Question 3//
+var question3 = document.getElementById("question3");
+var correct3 = document.getElementById("correct3");
+var incorrect3 = document.getElementById("incorrect3");
+
+//Question 4//
+var question4 = document.getElementById("question4");
+var correct4 = document.getElementById("correct4");
+var incorrect4 = document.getElementById("incorrect4");
+
+//Question 5//
+var question5 = document.getElementById("question5");
+var correct5 = document.getElementById("correct5");
+var incorrect5 = document.getElementById("incorrect5");
+
+//Question 6//
+var question6 = document.getElementById("question6");
+var correct6 = document.getElementById("correct6");
+var incorrect6 = document.getElementById("incorrect6");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Not sure if I want to use this code below since I changed the HTML format//
 var quizCard = document.getElementById("quiz-card");
-var startBtn = document.getElementById("start")
 
 
 //Creating the 'time' variable//
