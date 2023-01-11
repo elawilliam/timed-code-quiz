1827c9f3a60f5ea08ecd157342790b458340a9dd
Angela Witham <ela.witham@gmail.com>
2023-01-11 16:13:24 -0700

    timer functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,16 @@ var questions = [
     "When a function is declared as const, it can be called on any type of object"
 ];
 
+//Start button//
+var startBtn = document.querySelector("#start");
+
 
 //Creating the 'time' variable//
 var time = document.getElementById("clock");
 time.textContent = "00";
- 
- 
+
+//User given 60 seconds to complete quiz//
+var secondsLeft = 60;
  
 //Function to start clock//
 function startClock() {
@@ -21,34 +25,24 @@ function startClock() {
        secondsLeft--;
        time.textContent = secondsLeft;
  
-       if (secondsLeft === 60) {
+       if (secondsLeft === 61) {
            clearInterval(timer);
-           time.textContent = "00";
+           time.textContent = " 00";
        }
  
        if (secondsLeft === 0) {
            clearInterval(timer);
+           time.textContent = "Quiz Over!"
            quizEnd();
        }
    }, 1000);
 }
+
+startBtn.addEventListener("click", startClock);
  
-//Function to end quiz//
-function quizEnd() {
+//Function to end quiz function quizEnd()//
  
-}
+//Function for each question function questionOne() function questionTwo() function questionThree() function questionFour() function questionFive() function questionSix()//
  
-//Function for each question//
-function questionOne()
-function questionTwo()
-function questionThree()
-function questionFour()
-function questionFive()
-function questionSix()
- 
-//Function to start quiz//
-function startQuiz() {
-   startClock();
-   questionOne();
-};
+//Function to start quiz function startQuiz() startClock();questionOne();//
 
