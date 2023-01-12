5598017762e011bcbbcfff690ba1a5024bb1e4d0
Angela Witham <ela.witham@gmail.com>
2023-01-11 17:52:17 -0700

    updated javascript with true/false buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,47 @@
-//Questions//
+//Where the intro & questions appear//
+var introCard = document.getElementById("intro-card");
+var questionsCard = document.getElementById("questions-card");
+
+//Questions array//
 var questions = [
     "Special characters are their own JavaScript data type",
-    "Local Variables are visible everywhere in your JavaScript code",
+    "Local Variables are visible everywhere in JavaScript code",
     "Boolean data types in JavaScrip are True/False",
     "pop() removes the last item in an array",
     "JavaScript is not a case-sensitive language",
     "When a function is declared as const, it can be called on any type of object"
 ];
 
+//Correct answers array//
+var correctAnswers = [
+    "False",
+    "False",
+    "True",
+    "True",
+    "False",
+    "True"
+];
+
+//Incorrect answers array//
+var incorrectAnwers = [
+    "True",
+    "True",
+    "False",
+    "False",
+    "True",
+    "False"
+]
+
+//Creating True & False buttons//
+var trueBtn = document.createElement("button");
+var falseBtn = document.createElement("button");
+trueBtn.textContent = "True"
+falseBtn.textContent = "False"
+trueBtn.setAttribute("style", "width:100px; margin:50px; border-radius: 20px; background-color: white; cursor:pointer")
+falseBtn.setAttribute("style", "width:100px; margin:50px; border-radius: 20px; background-color: white; cursor:pointer")
+
 //Start button//
 var startBtn = document.querySelector("#start");
-
 
 //Creating the 'time' variable//
 var time = document.getElementById("clock");
@@ -37,12 +68,27 @@ function startClock() {
        }
    }, 1000);
 }
-
-startBtn.addEventListener("click", startClock);
  
 //Function to end quiz function quizEnd()//
  
-//Function for each question function questionOne() function questionTwo() function questionThree() function questionFour() function questionFive() function questionSix()//
+function askQuestion(){
+    introCard.textContent = "True or False?";
+    questionsCard.textContent = questions[1];
+    questionsCard.appendChild(trueBtn);
+    questionsCard.appendChild(falseBtn);
+
+
+}
+//Function for each question//
+function questionOne(){
+
+}
  
-//Function to start quiz function startQuiz() startClock();questionOne();//
+//Function to start quiz//
+function startQuiz(){
+    startClock();
+    askQuestion();}
+
+//Event listener for starting quiz// 
+startBtn.addEventListener("click", startQuiz);
 
