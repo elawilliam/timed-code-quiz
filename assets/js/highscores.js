5f892fd1c3958353ba2b2250bef2f3467cc7e928
Angela Witham <ela.witham@gmail.com>
2023-02-17 12:42:15 -0700

    added README

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,15 +1,14 @@
 var highScores = document.getElementById("high-scores");
 var playerScores = JSON.parse(localStorage.getItem("playerScores")) || [];
 
+//function to show high scores with initials & score//
 function renderPlayers() {
     for (var i = 0; i < playerScores.length; i++) {
-        // var player = JSON.parse(localStorage.getItem("storageArray"));
         console.log(playerScores[i]);
         var playerScoreElement = document.createElement("p");
         playerScoreElement.textContent = `${playerScores[i].playerInitials} ${playerScores[i].timeLeft}`;
         highScores.appendChild(playerScoreElement);
     }
 };
-    //sort players here before rendering//
-    renderPlayers();
+renderPlayers();
 
